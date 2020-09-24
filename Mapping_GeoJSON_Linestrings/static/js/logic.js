cf8b606a98abefeecce64fda7dac627e3d7c2864
Rabia zia <zrabiazia@gmail.com>
2020-09-24 10:18:41 -0600

    uploading

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -2,9 +2,10 @@
 //let map = L.map('mapid').setView([30, 30], 2);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-preview-night-v4/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
+    
     //id: 'mapbox/streets-v11',
     accessToken: API_KEY
 });
@@ -12,30 +13,31 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 //streets.addTo(map);
 
 // We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-preview-day-v4/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
+    
     accessToken: API_KEY
 });
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Street: streets,
+    Light: light,
     Dark: dark
   };
 
   // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [30, 30],
+    center: [40.0, -80.0],
     zoom: 2,
-    layers: [streets]
+    layers: [light]
 })
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
 //accessing the airport GeoJSON URL
 
-let airportData = "https://raw.githubusercontent.com/zrabiazia/Mapping_earthquakes/master/majorAirports.json";
+let torontoData = "https://raw.githubusercontent.com/zrabiazia/Mapping_earthquakes/master/torontoRoutes.json";
 // Add GeoJSON data.
 //let sanFranAirport =
 //{"type":"FeatureCollection","features":[{
@@ -64,15 +66,27 @@ let airportData = "https://raw.githubusercontent.com/zrabiazia/Mapping_earthquak
      //   return L.marker(latlng).bindPopup("<h2>"+feature.properties.name + "<br> "+ "_______________________________" + "<br>" + feature.properties.city +", " + feature.properties.country +"</h2.");
     //}
 //}).addTo(map);
+// Grabbing our GeoJSON data.
+d3.json(torontoData).then(function(data) {
+     console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data, {
+    color:"#ffffa1",
+    weight : 2,
+    onEachFeature: function(feature, layer){
+      layer.bindPopup("<h3>Airline:" + feature.properties.airline + "</h3><hr><h3>Destination:" + feature.properties.dst + "</h3>");
+    }
+  }).addTo(map);
+});
 
 //Grabbing our JSON Data 
-d3.json(airportData).then(function(data) {
-    console.log(data);
+//d3.json(torontoData).then(function(data) {
+  //  console.log(data);
     //creating a GeoJSON layer with the retrieved data
-    L.geoJson(data, {
-        pointToLayer: function(feature, latlng){
-            console.log(feature);
-            return L.marker(latlng).bindPopup("<h2>"+ "Airport Code: " +  feature.properties.faa + "<br> "+ "_______________________________" + "<br>" +"Airport name: "+ feature.properties.name +"</h2.");
-        }
-    }).addTo(map);
-});
+    //L.geoJson(data, {
+      //  pointToLayer: function(feature, latlng){
+        //    console.log(feature);
+          //  return L.marker(latlng).bindPopup("<h2>"+ "Airport Code: " +  feature.properties.faa + "<br> "+ "_______________________________" + "<br>" +"Airport name: "+ feature.properties.name +"</h2.");
+        //}
+    //}).addTo(map);
+//});
