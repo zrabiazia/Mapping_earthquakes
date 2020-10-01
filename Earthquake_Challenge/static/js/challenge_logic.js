15db53e904984fc60e67eb6026b2c6d90ff73974
Rabia zia <zrabiazia@gmail.com>
2020-09-30 23:11:43 -0600

    uploading challenge

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -147,7 +147,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geoj
     if (magnitude < 4) {
       return "#ee9c00";
     }
-    return "#ea2c2c"
+    return "#ea2c2c";
   }
 
 // 6. Use the function that determines the radius of the earthquake marker based on its magnitude.
